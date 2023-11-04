1a394df1fa392779bfa50a65a9bc28583c55cdc7
ajithreddy1441 <syamalaajith11@gmail.com>
2023-11-04 17:51:04 +0530

    created

--- mail.js
+++ mail.js
@@ -26,13 +26,6 @@ function submitForm(e) {
 
   saveMessages(name, emailid, msgContent);
 
-  //   enable alert
-  document.querySelector(".alert").style.display = "block";
-
-  //   remove the alert
-  setTimeout(() => {
-    document.querySelector(".alert").style.display = "none";
-  }, 3000);
 
   //   reset the form
   document.getElementById("contactForm").reset();
